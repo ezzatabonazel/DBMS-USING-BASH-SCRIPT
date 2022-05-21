95b27754a5829775d8b278f9d348a2778085eb3a
Mahmoud Rizk <mrizk@localhost.localdomain>
2022-05-22 01:25:19 +0200

    last edit

--- createtable.sh
+++ createtable.sh
@@ -24,5 +24,43 @@ else
     echo "ERROR: Table name can not be empty"
 fi
 
+# creat column 
+
+read -p " Enter number of columns: " colnum
+
+
+for (( i=0; i<$colnum ; i++))
+do
+      colmeta=""
+      read -p "enter column name" colname
+      if [-z $colname ]
+      then 
+            echo " colname cannot be empty "
+      elif [[ $colname=~ ^[0-9] ]]
+      then 
+            echo "Colname is not correct "
+      else
+            colmeta=$colname
+      fi
+      read -p "choice column data type string(s) number(n) [s:n]    " coldatatype
+      case $coldatatype in 
+      "s" | "S") 
+                  colmeta=$colmeta:string 
+                  ;;
+      "n" | "N") 
+                  colmeta=$colmeta:number
+                  ;;
+      esac
+      read -p " is that column primary key (PK) ? [y/n]  " pk
+      case $pk in 
+      "y" | "Y")
+                  colmeta=$colmeta:yes
+                  ;;
+      "n" | "N")  colmeta=$colmeta:no
+                  ;;
+      esac
+echo $colmeta >> $HOME/db/Databases/$ctdb/metadata/$tname 
+
+done
 
 
