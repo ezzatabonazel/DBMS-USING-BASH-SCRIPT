003b6b2f8a688ecdccb6edfe55caf06f5d65004e
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-05-21 16:07:52 -0400

    update

--- createtable.sh
+++ createtable.sh
@@ -3,20 +3,23 @@
 # Get the table name from user 
 read -p "what is your table name" tname 
 
-if ! [ -z $tname ]                                                #first check
+if ! [ -z $tname ]                                                      #first check
 then
-   if[[$tname =~ ^[0-9]]]                                         #second check 
-   then
-         echo " sorry invalid table name"
-   elif[ -f $HOME/db/Databases && /Databases/Data ]                        #Third check 
-         echo "sorry the table is already exists"
-   else                                            
-     touch $HOME/db/Databases/$ctdb/Data/$tname     #create file
-     touch $HOME/db/Databases/$ctdb/metadata/$tname 
-     echo "congrats your table is created successfully"
-   fi
+      if[[ $tname =~ ^[0-9] ]]                                          #second check 
+      then
+
+         echo " ERROR: invalid table name"
+      
+      elif[ -f $HOME/db/Databases/$ctdb/metadata/$tname ] && [ -f $HOME/db/Databases/$ctdb/Data/$tname ]                  #Third check 
+            
+            echo "ERROR: the table is already exists"
+      else                                            
+            touch $HOME/db/Databases/$ctdb/Data/$tname                        #create file
+            touch $HOME/db/Databases/$ctdb/metadata/$tname 
+            echo "congrats your table is created successfully"
+      fi
 else
-    echo "please enter your table name "
+    echo "ERROR: Table name can not be empty"
 fi
 
 
