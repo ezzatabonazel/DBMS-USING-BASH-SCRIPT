c9a2f832e586ddfe8ee0c7eefac97a9751be6349
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-05-27 12:33:27 -0400

    last update

--- deletedb.sh
+++ deletedb.sh
@@ -8,16 +8,22 @@ then
 	echo "YOU MUST ENTER A NAME"
 elif [ -d ./Databases/$name ]
 then	
-	read -p " Are you sure to delete $name database [y-n] : " check
-	case $check in 
-		"y"|"Y")
-			rm -r ./Databases/$name
-			echo " Your Database is deleted successfully "
-			;;
-		"n"|"N")
-			exit
-			;;
-	esac
+	while true : 
+	do
+		read -p " Are you sure to delete $name database [y/n] : " check
+		case $check in 
+			"y"|"Y")
+				rm -r ./Databases/$name
+				echo " Your Database is deleted successfully "
+				break;
+				;;
+			"n"|"N")
+							exit
+							;;
+				*) echo "invalid choice :("
+							;;
+		esac
+	done
 else 
 	echo "This Database deosn't exist"
 fi
