b5c927801b963f3da2af46f8a802bfc5b77c3f5b
ezzatabonazel <ezzat.kadry.abonazel@gmail.com>
2022-06-07 16:25:00 -0400

    exist check

--- createdb.sh
+++ createdb.sh
@@ -8,14 +8,19 @@ do
 
 	read -p " what is your Datebase name : " dbname
 
-	if ! [[ -z $dbname ]]
-	then 
-	#check of DBname does't begin with number and at least contain one character
+	if [[ -z $dbname ]]
+	then
+			echo "PLEASE ENTER A NAME FOR YOUR DATABASE"
+	elif [ -d ./Databases/$dbname ]
+	then
+		       echo "this database is existed"
+	else
+
 			case $dbname in
 					+([a-zA-Z_-]) )		
-						mkdir ./Databases/$dbname
-						mkdir ./Databases/$dbname/Data
-						mkdir ./Databases/$dbname/metadata
+						mkdir Databases/$dbname
+						mkdir Databases/$dbname/Data
+						mkdir Databases/$dbname/metadata
 						echo "your Database is created successfully"
 						break
 						;;
@@ -25,10 +30,6 @@ do
 						echo "invalid Database name ,must contain only characters :("
 						;;
 			esac
-		
-	else	
-
-	echo "PLEASE ENTER A NAME FOR YOUR DATABASE"
-	fi
+		fi
 
 done
